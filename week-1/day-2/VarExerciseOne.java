f3f0d5d9606b20126209b1ed85a20a161b21972d
TheChronicMonster <legendzing@gmail.com>
2019-06-04 15:56:10 -0500

    add: var exercises

---
+++ week-1/day-2/VarExerciseOne.java
@@ -0,0 +1,17 @@
+class VarExerciseOne {
+    public static void main (String args[]) {
+        int integerVariable;
+        double doubleVariable;
+        String stringVariable;
+        
+        integerVariable = 256;
+        doubleVariable = 3.1415;
+        stringVariable = "I'm creating variables.";
+         
+        System.out.print("My favorite integer is ");
+        System.out.println(integerVariable);
+        System.out.print("A popular decimal is pi. It starts as ");
+        System.out.println(doubleVariable);
+        System.out.println(stringVariable);
+    }
+}
