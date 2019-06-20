e2ef08cc5080d30f636cda3928f6e18c6c5e3470
TheChronicMonster <legendzing@gmail.com>
2019-06-20 10:38:36 -0500

    add: new concatenation exercises

---
+++ week-1/day-2/concatenation/IntroToConcatenation.java
@@ -0,0 +1,15 @@
+public class IntroToConcatenation {
+    public static void main(String[] args) {
+        String concatenateString = "2" + "2";
+        System.out.println(concatenateString);
+
+        String makeAPhrase = "made" + " " + "by" + " " + "them";
+        String maydm = "Maydm" + " " + "is";
+        String end = "I" + " " + "made" + " " + "this" + " " + "with" + " " + "Maydm!";
+
+        System.out.println(maydm);
+        System.out.print(makeAPhrase);
+        System.out.print("\n");
+        System.out.print(end);
+    }
+}
