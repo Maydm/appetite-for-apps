aed2285bf4102294568146417c15edb953afd477
TheChronicMonster <legendzing@gmail.com>
2019-06-21 09:20:01 -0500

    add: string concatenation exercises

---
+++ week-1/day-3/strings/concatenation/StringConcatenater.java
@@ -0,0 +1,17 @@
+public class StringConcatenater {
+    public static void main(String[] args) {
+        String words, moreWords;
+        words = "";
+        moreWords = "";
+
+        words = words + "tom ";
+        words = words + "purple ";
+        words = words + "greece ";
+        System.out.println(words);
+
+        moreWords += "time ";
+        moreWords += "check ";
+        moreWords += "lobby ";
+        System.out.println(moreWords);
+    }
+}
